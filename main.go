1a82e14485e9d82ad653fd5ef33f622c6d41e2d1
rlarudgkswkd <khkim6@eduwill.net>
2020-03-25 14:34:08 +0900

    first commit

--- main.go
+++ main.go
@@ -8,35 +8,49 @@ import (
 
 func main() {
 
-	var fileSave string = "10000_20_"
-	num := 0
+	var fileSave string = "9000_20_"
+	num := 0.0
+	arr1 := make([]float64, 0)
+	var averRage float64
 
-	file1, _ := os.Open("10000 20 9021.txt") // hello1.txt 파일 열기
-	defer file1.Close()                      // main 함수가 끝나기 직전에 파일을 닫음
+	//set the variables
+	diviNum := 20.0 //몇으로 나눌지
+	setPartialNum := 20
+	setWholeNum := 1200
+	theOpenWay := "C:\\Users\\khkim6\\Desktop\\testingcase\\9000_20_7255.txt"
+	theCreateWay := "C:\\Users\\khkim6\\Desktop\\testingcase\\90000_20_7225\\"
+	theSaveAvgWay := "C:\\Users\\khkim6\\Desktop\\testingcase\\aveRage9000_20.txt"
+
+	file1, _ := os.Open(theOpenWay) // hello1.txt 파일 열기
+	defer file1.Close()             // main 함수가 끝나기 직전에 파일을 닫음
 
 	i := 0
 	filenum := 1
 	//s := make([]int, 0)
 
 	for {
-		file2, _ := os.Create("C:\\Users\\khkim6\\go\\src\\github.com\\Beautykkh\\learngo\\readmyfile\\analysis\\10000-20-9021\\" + fileSave + strconv.Itoa(filenum) + ".txt")
+		file2, _ := os.Create(theCreateWay + fileSave + strconv.Itoa(filenum) + ".txt")
 
-		for j := 0; j < 20; j++ {
+		for j := 0; j < setPartialNum; j++ {
 			fmt.Fscan(file1, &num)
 			fmt.Print("i:", i)
 			fmt.Print(" num:", num, "\n")
 			fmt.Fprintln(file2, num)
+			averRage = averRage + num
 
 			i++
+
 		}
 
-		if i%20 == 0 {
+		if i%setPartialNum == 0 {
 			//s = make([]int, 0)
+			arr1 = append(arr1, averRage/diviNum) //calculate the average
+			averRage = 0.0
 			filenum++
 			file2.Close()
 		}
 
-		if i == 1200 {
+		if i == setWholeNum {
 			break
 		}
 
@@ -49,5 +63,19 @@ func main() {
 		i++*/
 
 	}
+	var selection string
+
+	fmt.Printf("Do you want to save? -> Y OR N :")
+	fmt.Scanf("%s", &selection)
+
+	if selection == "Y" {
+		file3, _ := os.Create(theSaveAvgWay)
+		defer file3.Close()
+		for _, num := range arr1 {
+			fmt.Fprintln(file3, num)
+		}
+	}
+
+	fmt.Println(arr1)
 
 }
